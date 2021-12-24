b82cdb5543ef62a86d104473d1b55b7184d0aca8
Juan Carlos Trimino <juancarlos@trimino.com>
2021-12-23 22:56:09 -0600

    Reformat the code.

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -23,6 +23,29 @@ const MAX_RETRIES = process.env.MAX_RETRIES && parseInt(process.env.MAX_RETRIES)
 let READINESS_PROBE = false;
 
 /***
+Resume Operation
+----------------
+The resume operation strategy intercepts unexpected errors and responds by allowing the process to
+continue.
+***/
+process.on("uncaughtException",
+err => {
+  console.error("Uncaught exception:");
+  console.error(err && err.stack || err);
+})
+
+/***
+Abort and Restart
+-----------------
+***/
+// process.on("uncaughtException",
+// err => {
+//   console.error("Uncaught exception:");
+//   console.error(err && err.stack || err);
+//   process.exit(1);
+// })
+
+/***
 Unlike most other programming languages or runtime environments, Node.js doesn't have a built-in
 special "main" function to designate the entry point of a program.
 
@@ -31,55 +54,44 @@ Accessing the main module
 When a file is run directly from Node.js, require.main is set to its module. That means that it is
 possible to determine whether a file has been run directly by testing require.main === module.
 ***/
-if (require.main === module)
-{
+if (require.main === module) {
   main()
-  .then(() =>
-  {
+  .then(() => {
     READINESS_PROBE = true;
     console.log(`Microservice "video-streaming" is listening on port "${PORT}"!`);
   })
-  .catch(err =>
-  {
+  .catch(err => {
     console.error('Microservice "video-streaming" failed to start.');
     console.error(err && err.stack || err);
   });
 }
 
-function main()
-{
+function main() {
   //Throw an exception if any required environment variables are missing.
-  if (process.env.SVC_DNS_RABBITMQ === undefined)
-  {
+  if (process.env.SVC_DNS_RABBITMQ === undefined) {
     throw new Error('Please specify the name of the service DNS for RabbitMQ in the environment variable SVC_DNS_RABBITMQ.');
   }
-  else if (process.env.SVC_DNS_VIDEO_STORAGE === undefined)
-  {
+  else if (process.env.SVC_DNS_VIDEO_STORAGE === undefined) {
     throw new Error('Please specify the service DNS in the environment variable SVC_DNS_VIDEO_STORAGE.');
   }
   //Display a message if any optional environment variables are missing.
-  else
-  {
-    if (process.env.PORT === undefined)
-    {
+  else {
+    if (process.env.PORT === undefined) {
       console.log('The environment variable PORT for the "HTTP server" is missing; using port 3000.');
     }
     //
-    if (process.env.MAX_RETRIES === undefined)
-    {
+    if (process.env.MAX_RETRIES === undefined) {
       console.log(`The environment variable MAX_RETRIES is missing; using MAX_RETRIES=${MAX_RETRIES}.`);
     }
   }
   //Notify when server has started.
   return requestWithRetry(connectToRabbitMQ, SVC_DNS_RABBITMQ, MAX_RETRIES)  //Connect to RabbitMQ...
-  .then(channel =>                    //then...
-  {
+  .then(channel => {                  //then...
     return startHttpServer(channel);  //start the HTTP server.
   });
 }
 
-function connectToRabbitMQ(url, currentRetry)
-{
+function connectToRabbitMQ(url, currentRetry) {
   console.log(`Connecting (${currentRetry}) to 'RabbitMQ' at ${url}.`);
   /***
   return connect()
@@ -99,44 +111,34 @@ function connectToRabbitMQ(url, currentRetry)
     });
   ***/
   return amqp.connect(url)
-  .then(conn =>
-  {
+  .then(conn => {
     console.log("Connected to RabbitMQ.");
     //Create a RabbitMQ messaging channel.
     return conn.createChannel()
-    .then(channel =>
-    {
+    .then(channel => {
       //Assert that we have a "viewed" queue.
       return channel.assertQueue('viewed', { exclusive: false })
-      .then(() =>
-      {
+      .then(() => {
         return channel;
       });
     });
   });
 }
 
-async function sleep(timeout)
-{
-  return new Promise(resolve =>
-  {
+async function sleep(timeout) {
+  return new Promise(resolve => {
     setTimeout(() => { resolve(); }, timeout);
   });
 }
 
-async function requestWithRetry(func, url, maxRetry)
-{
-  for (let currentRetry = 0;;)
-  {
-    try
-    {
+async function requestWithRetry(func, url, maxRetry) {
+  for (let currentRetry = 0;;) {
+    try {
       ++currentRetry;
       return await func(url, currentRetry);
     }
-    catch(err)
-    {
-      if (currentRetry === maxRetry)
-      {
+    catch(err) {
+      if (currentRetry === maxRetry) {
         //Save the error from the most recent attempt.
         lastError = err;
         console.log(`Maximum number of ${maxRetry} retries has been reached.`);
@@ -152,15 +154,12 @@ async function requestWithRetry(func, url, maxRetry)
 }
 
 //Start the HTTP server.
-function startHttpServer(channel)
-{
+function startHttpServer(channel) {
   //Notify when the server has started.
-  return new Promise(resolve =>
-  {
+  return new Promise(resolve => {
     setupHandlers(channel);
     app.listen(PORT,
-    () =>
-    {
+    () => {
       //HTTP server is listening, resolve the promise.
       resolve();
     });
@@ -168,31 +167,26 @@ function startHttpServer(channel)
 }
 
 //Setup event handlers.
-function setupHandlers(channel)
-{
+function setupHandlers(channel) {
   //Readiness probe.
   app.get('/readiness',
-  (req, res) =>
-  {
+  (req, res) => {
     res.sendStatus(READINESS_PROBE === true ? 200 : 500);
   });
   //
   //Route for streaming video.
   app.get('/video',
-  (req, res) =>
-  {
+  (req, res) => {
     const videoId = req.query.id;
     console.log(`Request ${videoId} from the video storage service.`);
     //Forward the request to the video storage microservice.
-    const forwardReq = http.request(
-    {
+    const forwardReq = http.request({
       host: SVC_DNS_VIDEO_STORAGE,
       path: `/video?id=${videoId}`,
       method: 'GET',
       headers: req.headers
     },
-    forwardRes =>
-    {
+    forwardRes => {
       res.writeHeader(forwardRes.statusCode, forwardRes.headers);
       forwardRes.pipe(res);
     });
@@ -214,8 +208,7 @@ function sendMultipleRecipientMessage(channel, videoId)
 }
 ***/
 
-function sendSingleRecipientMessage(channel, videoId)
-{
+function sendSingleRecipientMessage(channel, videoId) {
   console.log('Publishing message on "viewed" queue.');
   //Define the message payload. This is the data that will be sent with the message.
   const msg = { video: { id: videoId } };
